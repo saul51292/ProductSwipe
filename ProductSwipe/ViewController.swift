13ad5185397e1994e74fd9ac4a737139121a02b2
saul51292 <saulhamadani@me.com>
2015-04-29 18:23:34 -0400

    Initial Commit

---
+++ ProductSwipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductSwipe
+//
+//  Created by Saul on 4/29/15.
+//  Copyright (c) 2015 Saul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
